b8db15bae297d8b04e2c31ea193b9543d9305ff2
GM2mars <gm2mars@gmail.com>
2026-01-09 15:25:31 +0200

    test adaptive viewport

--- index.f9ba7.js
+++ index.f9ba7.js
@@ -1,4 +1,4 @@
-System.register(["./application.d4d3e.js"], function (_export, _context) {
+System.register(["./application.8a035.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

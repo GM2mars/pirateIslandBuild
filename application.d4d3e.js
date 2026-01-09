b9527b9b3750f215b265941b093a66c24d9bc62f
GM2mars <gm2mars@gmail.com>
2026-01-09 15:17:06 +0200

    fix viewport

--- application.d4d3e.js
+++ application.d4d3e.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.65c49.json';
+          this.settingsPath = 'src/settings.eb96c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.57978.js
+++ index.57978.js
@@ -1,4 +1,4 @@
-System.register(["./application.8734e.js"], function (_export, _context) {
+System.register(["./application.d4d3e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

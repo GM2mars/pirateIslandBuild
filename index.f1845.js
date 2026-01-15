3ec6a3d9b218a4d9f82954fa6f2fd8d62d74fa43
GM2mars <gm2mars@gmail.com>
2026-01-15 16:32:10 +0200

    fix respawn birds

--- index.f1845.js
+++ index.f1845.js
@@ -1,4 +1,4 @@
-System.register(["./application.74969.js"], function (_export, _context) {
+System.register(["./application.a5f3b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

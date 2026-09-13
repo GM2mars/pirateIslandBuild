38ceeb9c8025a8f267947540dfc935bd24af62d3
GM2mars <gm2mars@gmail.com>
2026-09-13 21:37:15 +0300

    fix build

--- index.96b45.js
+++ index.96b45.js
@@ -1,4 +1,4 @@
-System.register(["./application.ea575.js"], function (_export, _context) {
+System.register(["./application.b78f0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

d5c27279ae44adf12f76a6804aea700ddf76f10f
GM2mars <gm2mars@gmail.com>
2026-09-15 21:48:10 +0300

    new build

--- index.a77e9.js
+++ index.a77e9.js
@@ -1,4 +1,4 @@
-System.register(["./application.9cfd1.js"], function (_export, _context) {
+System.register(["./application.7e5b7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

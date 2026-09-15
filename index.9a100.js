c45eb794c68daf7080ab10b2033416675c3240a1
GM2mars <gm2mars@gmail.com>
2026-09-15 20:27:53 +0300

    build

--- index.9a100.js
+++ index.9a100.js
@@ -1,4 +1,4 @@
-System.register(["./application.93f5b.js"], function (_export, _context) {
+System.register(["./application.32f89.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

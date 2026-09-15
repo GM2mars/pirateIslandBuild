47d757ed30fda86943fde3e20e593539885bd514
GM2mars <gm2mars@gmail.com>
2026-09-15 21:06:14 +0300

    new build

--- index.d0149.js
+++ index.d0149.js
@@ -1,4 +1,4 @@
-System.register(["./application.8a792.js"], function (_export, _context) {
+System.register(["./application.9cfd1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

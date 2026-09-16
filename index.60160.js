a839078595968f73c8ab018bb6b5a2e36ae5215e
GM2mars <gm2mars@gmail.com>
2026-09-16 20:19:06 +0300

    new build

--- index.60160.js
+++ index.60160.js
@@ -1,4 +1,4 @@
-System.register(["./application.3b156.js"], function (_export, _context) {
+System.register(["./application.8d22f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

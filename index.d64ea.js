52b14564915973139e76d29f413d355cbf64bd37
GM2mars <gm2mars@gmail.com>
2026-09-16 12:20:03 +0300

    fix mission

--- index.d64ea.js
+++ index.d64ea.js
@@ -1,4 +1,4 @@
-System.register(["./application.7e5b7.js"], function (_export, _context) {
+System.register(["./application.3b156.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

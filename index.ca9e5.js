d45e9c612b3d5fa7773ac268da0466ea3bf4e858
GM2mars <gm2mars@gmail.com>
2026-09-16 23:41:54 +0300

    fix buld

--- index.ca9e5.js
+++ index.ca9e5.js
@@ -1,4 +1,4 @@
-System.register(["./application.cdd4a.js"], function (_export, _context) {
+System.register(["./application.87c00.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

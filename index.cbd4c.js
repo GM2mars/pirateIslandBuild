8313509f960446c4f0eaf5c8c1d36ccba117eec4
GM2mars <gm2mars@gmail.com>
2026-09-17 18:54:49 +0300

    new build

--- index.cbd4c.js
+++ index.cbd4c.js
@@ -1,4 +1,4 @@
-System.register(["./application.87c00.js"], function (_export, _context) {
+System.register(["./application.368ac.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

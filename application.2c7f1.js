3e581858b2327c076c5bf2c802994399abdaa1b3
GM2mars <gm2mars@gmail.com>
2026-09-22 21:52:05 +0300

    new build

--- application.2c7f1.js
+++ application.2c7f1.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.0a5c2.json';
+          this.settingsPath = 'src/settings.41e53.json';
           this.showFPS = false;
         }
         _createClass(Application, [{

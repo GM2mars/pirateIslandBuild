ea9688b3f11094c7c9ab737f43d7e54a75347756
GM2mars <gm2mars@gmail.com>
2026-09-24 22:03:31 +0300

    new global build

--- application.04cae.js
+++ application.04cae.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.41e53.json';
+          this.settingsPath = 'src/settings.7ee8f.json';
           this.showFPS = false;
         }
         _createClass(Application, [{

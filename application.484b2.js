49afe47b6f778035d5d52df0831b9e1b4acea134
GM2mars <gm2mars@gmail.com>
2026-09-25 17:49:15 +0300

    new build

--- application.484b2.js
+++ application.484b2.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.7ee8f.json';
+          this.settingsPath = 'src/settings.c04e0.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.53aa6.js
+++ index.53aa6.js
@@ -1,4 +1,4 @@
-System.register(["./application.04cae.js"], function (_export, _context) {
+System.register(["./application.484b2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

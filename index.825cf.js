3a915a2ffa312857d5f27b81d72c5dc27d1c848b
GM2mars <gm2mars@gmail.com>
2026-09-25 23:06:50 +0300

    new build

--- index.825cf.js
+++ index.825cf.js
@@ -1,4 +1,4 @@
-System.register(["./application.484b2.js"], function (_export, _context) {
+System.register(["./application.a2550.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
